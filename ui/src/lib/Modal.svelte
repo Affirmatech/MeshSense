14662cc2c59e9f3ade102711f13579a0e079ac4f
Christopher Davis <Christopher.Davis.013@gmail.com>
2024-08-02 17:46:13 -0400

    Modal dark theme only

--- ui/src/lib/Modal.svelte
+++ ui/src/lib/Modal.svelte
@@ -20,11 +20,11 @@
     id="popover-default"
     role="tooltip"
     class="{$$restProps.class || ''} fixed z-20 w-[80%] left-[10%] top-[10%] max-h-[80%] flex flex-col
-    text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800"
+    text-sm transition-opacity duration-300 border rounded-lg shadow-sm text-gray-400 border-gray-600 bg-gray-800"
   >
-    <div class="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+    <div class="px-3 py-2 border-b rounded-t-lg border-gray-600 bg-gray-700">
       {#if title != undefined}
-        <h3 class="font-semibold text-gray-900 dark:text-white flex">
+        <h3 class="font-semibold text-white flex">
           <div class="grow">
             {title}
           </div>
