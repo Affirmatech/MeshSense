0e0770bdc95e01c9cb0e7bd013ea7f11a03cecf8
Christopher Davis <Christopher.Davis.013@gmail.com>
2024-08-08 15:19:21 -0400

    Increase default resolution

--- electron/src/main/index.ts
+++ electron/src/main/index.ts
@@ -10,8 +10,8 @@ let apiPort: any = 9999
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 1300,
+    height: 900,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
