5f9369747d4c424ec5954cd3dfce1b50c22802b2
Christopher Davis <Christopher.Davis.013@gmail.com>
2024-09-03 14:00:54 -0400

    Display Node# and Firmware

--- ui/src/Address.svelte
+++ ui/src/Address.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { connectionStatus, address, enableTLS } from 'api/src/vars'
+  import { connectionStatus, address, enableTLS, myNodeMetadata, myNodeNum, nodes, lastFromRadio } from 'api/src/vars'
   import Card from './lib/Card.svelte'
   import { smallMode } from './Nodes.svelte'
   import { hasAccess } from './lib/util'
@@ -45,10 +45,22 @@
         </label>
         <button class="btn w-full h-full col-span-full saturate-150">Connect</button>
       {:else if $connectionStatus == 'connected'}
-        <button type="button" class="btn w-full h-full" on:click={disconnect}>Disconnect</button>
+        <button type="button" class="btn w-full h-full {$smallMode ? 'col-span-full' : ''}" on:click={disconnect}>Disconnect</button>
       {:else if ['searching', 'connecting', 'configuring', 'reconnecting'].includes($connectionStatus)}
         <button type="button" class="btn w-full h-full" on:click={disconnect}>Cancel</button>
       {/if}
+    {/if}
+    {#if $myNodeMetadata?.firmwareVersion}
+      <div class="grid gap-1 text-xs col-span-2 {$smallMode ? '' : 'grid-cols-2'}">
+        <div class="flex gap-2 items-center bg-black/30 rounded">
+          <h2 class="rounded">Node #</h2>
+          <div class="text-xs">{$myNodeNum}</div>
+        </div>
+        <div class="flex gap-2 items-center bg-black/30 rounded">
+          <h2 class="rounded">FW</h2>
+          <div class="text-xs">{$myNodeMetadata?.firmwareVersion}</div>
+        </div>
+      </div>
     {/if}
   </form>
 </Card>
