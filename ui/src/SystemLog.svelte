b963595696a19f2f0d31f5517eef23b851876efb
Christopher Davis <Christopher.Davis.013@gmail.com>
2024-09-06 10:15:44 -0400

    Auto Refreshing SystemLog

--- ui/src/SystemLog.svelte
+++ ui/src/SystemLog.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import axios from 'axios'
-  import { onMount, tick } from 'svelte'
+  import { onDestroy, onMount, tick } from 'svelte'
   import { scrollToBottom, userKey } from './lib/util'
   import { AnsiUp } from 'ansi_up'
   import { split } from 'postcss/lib/list'
@@ -10,7 +10,7 @@
   let log: string[] = []
   let logElement: HTMLPreElement
 
-  $: if (log && logElement) scrollToBottom(logElement, true)
+  $: if (log && logElement) scrollToBottom(logElement)
 
   async function getLog() {
     try {
@@ -22,9 +22,11 @@
   }
 
   onMount(getLog)
+  let refreshInterval = setInterval(getLog, 500)
+  onDestroy(() => clearInterval(refreshInterval))
 </script>
 
 <div class="flex flex-col gap-4">
   <pre bind:this={logElement} class="whitespace-pre-line overflow-auto h-80 rounded ring bg-black/20 p-4">{#each log as line}{@html ansi.ansi_to_html(line)}{/each}</pre>
-  <button class="btn btn-sm" on:click={getLog}>Refresh</button>
+  <button class="btn btn-sm" on:click={() => scrollToBottom(logElement, true)}>Scroll to bottom</button>
 </div>
