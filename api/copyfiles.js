edee48f62abc231e84194525c4631cb5eba35404
Christopher Davis <Christopher.Davis.013@gmail.com>
2024-09-09 16:50:15 -0400

    Fix copyfiles path for serialport mac build

--- api/copyfiles.js
+++ api/copyfiles.js
@@ -3,7 +3,7 @@
 let copyfiles = require('copyfiles')
 const { platform, arch } = require('os')
 
-let serialPath = `node_modules/@serialport/bindings-cpp/prebuilds/${platform()}-${arch()}/*`
+let serialPath = `node_modules/@serialport/bindings-cpp/prebuilds/${platform()}-*${arch()}/*`
 console.log({serialPath})
 copyfiles([serialPath, '.'], { up: 3 }, console.error)
 copyfiles([serialPath, '../electron/resources/'], { up: 3 }, console.error)
