2ae9a391e027c4f68fc7ade0a9c1123074a4fb64
mblauser <mikael.github@blauser.org>
2024-09-23 15:11:53 -0700

    display observed channel utilization %
    will display a thin 6px vertical colorized bar graph reporting observed channel utilization (as INT) for each node. Placement to the right of each node entry.

--- ui/src/Nodes.svelte
+++ ui/src/Nodes.svelte
@@ -131,15 +131,35 @@
               </div>
             {/if}
             <!-- Channel Utilization % -->
-            {#if node.deviceMetrics?.channelUtilization}
-              {let $thisNodeChUasINT    = Math.floor(node.deviceMetrics?.channelUtilization);
-               let $thisNodeChUcolorbar = ($thisNodeChUasINT < 15 ? 'green' : $thisNodeChUasINT < 25 ? 'yellow' : 'red')}
-              <div title="{$thisNodeChUasINT}% Channel Utilization"
-                class="absolute w-1.5 rounded bottom-1 top-1 right-0.5 overflow-hidden border border-white/20 flex flex-col"
-              >
-                <div class="grow"></div>
-                <div class="bg-green-500" style="height: {$thisNodeChUasINT}%;"></div>
-              </div>
+            {#if node?.deviceMetrics?.channelUtilization}
+              {#let channelUtilizationINT = Math.floor(node.deviceMetrics.channelUtilization)}
+                {#let scaledHeight = Math.min(channelUtilizationINT * 2, 100)}
+                {#let colorClass = 
+                  channelUtilizationINT < 15 ? 'green' : 
+                  channelUtilizationINT < 25 ? 'yellow' : 
+                  channelUtilizationINT < 35 ? 'orange' : 
+                  'red'
+                }
+                  <div 
+                    title="{channelUtilizationINT}% Observed Channel Utilization"
+                    class="absolute w-1.5 rounded bottom-1 top-1 right-0.5 overflow-hidden border border-white/20 flex flex-col"
+                    aria-label="Observed Channel Utilization: {channelUtilizationINT}%"
+                    aria-valuemin="0"
+                    aria-valuemax="50"
+                    aria-valuenow={channelUtilizationINT}
+                  >
+                    <div class="grow"></div>
+                    <div 
+                      class:bg-green-500={colorClass === 'green'}
+                      class:bg-yellow-500={colorClass === 'yellow'}
+                      class:bg-orange-500={colorClass === 'orange'}
+                      class:bg-red-500={colorClass === 'red'}
+                      style="height: {scaledHeight}%;"
+                    ></div>
+                  </div>
+                {/let}
+                {/let}
+              {/let}
             {/if}
           </div>
 
