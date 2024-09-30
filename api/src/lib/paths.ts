e604b2daa354982002898c0abbf13eecc08601e0
Christopher Davis <Christopher.Davis.013@gmail.com>
2024-09-30 10:32:25 -0400

    Create data directory on startup

--- api/src/lib/paths.ts
+++ api/src/lib/paths.ts
@@ -1,13 +1,19 @@
 import url from 'url'
 import { dirname, sep, join } from 'path'
 import envPaths from 'env-paths'
+import { existsSync, fstat, mkdirSync } from 'fs'
 
 const userPaths = envPaths('meshsense', { suffix: '' })
 const __filename = url.fileURLToPath(import.meta.url)
 
 /** When in development, return the base `api` directory */
+
 export const programDirectory = dirname(__filename).replace(`api${sep}src${sep}lib`, `api`)
 export const staticDirectory = join(programDirectory, 'static')
 export const dataDirectory = userPaths.data
+
+if (!existsSync(dataDirectory)) {
+    mkdirSync(dataDirectory, {recursive: true})
+}
 
 console.log({ programDirectory, staticDirectory, dataDirectory })
