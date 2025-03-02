f06127121b81604f67ae657f72a0b7c75dc199c6
mblauser <mikael.github@blauser.org>
2025-03-02 15:24:52 -0800

    Update Nodes.svelte
    made each node representation relative HTML, to better display a vertical bar for each respective node

--- ui/src/Nodes.svelte
+++ ui/src/Nodes.svelte
@@ -83,7 +83,7 @@
     {#each $filteredNodes as node (node.num)}
       <div
         class:ring-1={node.hopsAway == 0}
-        class="bg-blue-300/10 rounded px-1 py-0.5 flex flex-col gap-0.5 {node.num == $myNodeNum
+        class="relative bg-blue-300/10 rounded px-1 py-0.5 flex flex-col gap-0.5 {node.num == $myNodeNum
           ? 'bg-gradient-to-r '
           : Date.now() - node.lastHeard * 1000 < ($nodeInactiveTimer ?? 60) * 60 * 1000
             ? ''
