5c727923e66a9fa1789a3f779734827a15101ea5
Christopher Davis <Christopher.Davis.013@gmail.com>
2025-03-05 10:26:09 -0500

    Directly return Tailwind Classes for ChannelUtilization

--- ui/src/lib/ChannelUtilization.svelte
+++ ui/src/lib/ChannelUtilization.svelte
@@ -1,42 +1,30 @@
 <script lang="ts">
-  import type { NodeInfo } from 'api/src/vars';
-  export let node: NodeInfo;
- 
-  $: channelUtilizationINT = node?.deviceMetrics?.channelUtilization 
-    ? Math.floor(node.deviceMetrics.channelUtilization) 
-    : null;
- 
-  $: scaledHeight = channelUtilizationINT !== null 
-    ? Math.min(channelUtilizationINT * 2, 100) 
-    : 0;
- 
-  $: colorClass = channelUtilizationINT !== null
-    ? (channelUtilizationINT < 15 ? 'green' : 
-       channelUtilizationINT < 20 ? 'greenyellow' :
-       channelUtilizationINT < 25 ? 'yellow' : 
-       channelUtilizationINT < 30 ? 'orange' : 
-       channelUtilizationINT < 35 ? 'red' :
-       'superred')
-    : 'grayscale'; // default color when channelUtilizationINT is null
+  import type { NodeInfo } from 'api/src/vars'
+  export let node: NodeInfo
+
+  $: channelUtilizationINT = node?.deviceMetrics?.channelUtilization ? Math.floor(node.deviceMetrics.channelUtilization) : null
+
+  $: scaledHeight = channelUtilizationINT !== null ? Math.min(channelUtilizationINT * 2, 100) : 0
+
+  function getColorClass(channelUtilizationINT: number) {
+    if (!channelUtilizationINT) return 'grayscale' // default color when channelUtilizationINT is null
+    if (channelUtilizationINT < 15) return 'bg-green-500'
+    if (channelUtilizationINT < 20) return 'bg-[#9acd32]'
+    if (channelUtilizationINT < 25) return 'bg-yellow-500'
+    if (channelUtilizationINT < 30) return 'bg-orange-500'
+    if (channelUtilizationINT < 35) return 'bg-red-500'
+    return 'bg-[red]'
+  }
 </script>
- 
+
 <div
-  title="{channelUtilizationINT !== null ? channelUtilizationINT + '% Observed Channel Utilization' : 'Channel Utilization Not Reported'}"
+  title={channelUtilizationINT !== null ? channelUtilizationINT + '% Observed Channel Utilization' : 'Channel Utilization Not Reported'}
   class="absolute w-1.5 rounded bottom-1 top-1 right-0.5 overflow-hidden border border-white/20 flex flex-col"
-  aria-label="{channelUtilizationINT !== null ? 'Observed Channel Utilization: ' + channelUtilizationINT + '%' : 'Channel Utilization Not Reported'}"
+  aria-label={channelUtilizationINT !== null ? 'Observed Channel Utilization: ' + channelUtilizationINT + '%' : 'Channel Utilization Not Reported'}
   aria-valuemin="0"
   aria-valuemax="50"
   aria-valuenow={channelUtilizationINT ?? 0}
 >
   <div class="grow"></div>
-  <div
-    class:bg-green-500={colorClass === 'green'}
-    class:bg-greenyellow-500={colorClass === 'greenyellow'}
-    class:bg-yellow-500={colorClass === 'yellow'}
-    class:bg-orange-500={colorClass === 'orange'}
-    class:bg-red-500={colorClass === 'red'}
-    class:bg-superred-500={colorClass === 'superred'}
-    class:bg-gray-500={colorClass === 'grayscale'}
-    style="height: {scaledHeight}%;"
-  ></div>
+  <div class={getColorClass(channelUtilizationINT)} style="height: {scaledHeight}%;"></div>
 </div>
