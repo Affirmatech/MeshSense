30cafd0a475bc31753af3f4f3f0be3bf42148c8a
Andy Shinn <andys@andyshinn.as>
2025-04-06 18:16:46 -0500

    basic menus with shortcuts on macOS

--- electron/src/preload/index.ts
+++ electron/src/preload/index.ts
@@ -1,8 +1,12 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
-const api = {}
+const api = {
+  onOpenSettings: (callback: () => void) => {
+    ipcRenderer.on('open-settings', callback)
+  }
+}
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
